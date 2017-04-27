4f94cbdd8b6945d72005052f8cd3da8dba411a4f
jack <jack@wanzi.email>
2017-04-27 15:30:26 +0800

    更改记住登录时间

--- app/Http/Middleware/UserLogin.php
+++ app/Http/Middleware/UserLogin.php
@@ -21,7 +21,7 @@ class UserLogin {
 					->where('id', $_COOKIE['user'])
 					->first();
 				if($info){
-					setcookie('user',$info->id,time()+60*60*24);
+					setcookie('user',$info->id,time()+60*60*24*7);
 					$request->session()->set('user', $info);
 				}
 				return $next($request);
