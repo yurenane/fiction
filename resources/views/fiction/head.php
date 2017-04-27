32b35eef23bb9c3f4e7e4cc4d0798a799fd21be3
jack <jack@wanzi.email>
2017-04-27 16:02:15 +0800

    禁止页面放大

--- resources/views/fiction/head.php
+++ resources/views/fiction/head.php
@@ -2,7 +2,7 @@
 <html lang="zh-cmn-Hans">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+		 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0,user-scalable=no"/>
 		<title>Fiction</title>
 		<link rel="stylesheet" href="<?php echo WEUI_CSS; ?>weui.css">
 		<link rel="stylesheet" href="<?php echo WEUI_CSS; ?>example.css">
