3ac878ecb656b6a9acf75a6ecac73b2d8affab64
jack <jack@wanzi.email>
2017-04-27 14:23:25 +0800

    修改菜单栏图片样式

--- resources/views/fiction/head.php
+++ resources/views/fiction/head.php
@@ -7,6 +7,12 @@
 		<link rel="stylesheet" href="<?php echo WEUI_CSS; ?>weui.css">
 		<link rel="stylesheet" href="<?php echo WEUI_CSS; ?>example.css">
 		<script src="<?php echo JS_PATH; ?>jquery-1.8.3.min.js"></script>
+		<style>
+		.weui-tabbar img{
+		      opacity: 0.5;
+		}	
+		
+		</style>
 	</head>
 	<body ontouchstart="">
 		<div class="container" id="container" style="position:static;">
